dc34169120af51e3e9513780a1b80c2a28455222
nihalGound <nsnihalgound123@gmail.com>
2024-11-09 23:16:04 +0530

    implement stripe payment

--- src/components/global/sidebar/workspace-placeholder.tsx
+++ src/components/global/sidebar/workspace-placeholder.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
-type Props = {
-    children: React.ReactNode
-}
+type Props = { children: React.ReactNode }
 
-const WorkspacePlaceholder = ({children}: Props) => {
+const WorkspacePlaceholder = ({ children }: Props) => {
   return (
     <span className="bg-[#545454] flex items-center font-bold justify-center w-8 px-2 h-7 rounded-sm text-[#1D1D1D]">
-        {children}
+      {children}
     </span>
   )
 }
